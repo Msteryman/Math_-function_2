318f969ef441c8f99d2bd1f770596342918c54f9
Georgi Balayan <123405107+Msteryman@users.noreply.github.com>
2024-01-31 23:18:00 +0400

    Update math_ algorithm.py
    Added function for determining points only by y points

--- Math_python_2/math_ algorithm.py
+++ Math_python_2/math_ algorithm.py
@@ -1,25 +1,54 @@
+import numpy as np
+from math_python import math_python
+
 def function_with_graf(intersection_points_x: list ,intersection_points_y: list):
     """Определяет уравнения по графику """
-    import numpy as np
+
+
     coefficient_matrix = []
     equation_ector = []
 
     for x,y in zip(intersection_points_x,intersection_points_y): # цыкл проходит по всем точками x и y
-        coefficient_matrix.append([x**2,x,1]) # записывает в  coefficient_matrix x в виде квадратного уравнения 
+        coefficient_matrix.append([x**2,x,1]) # записывает в coefficient_matrix x в виде квадратного уравнения 
         equation_ector.append(y) # записывает в  equation_ecto y
 
     A = np.array(coefficient_matrix) # Создания array A из coefficient_matrix
     B = np.array(equation_ector) # Создания array И из equation_ector
-    ans = np.linalg.solve(A,B) # считает систему уравнения матричным способам. Подробние про этот способ есть на этом сайте https://github.com/Coursera-machine-learning-data-analysis/1/blob/master/1.ipynb
+    print(A,B)
+    ans, residuals, rank, s = np.linalg.lstsq(A, B, rcond=None) # считает систему уравнения матричным способам. Подробние про этот способ есть на этом сайте https://github.com/Coursera-machine-learning-data-analysis/1/blob/master/1.ipynb
     ans = list(ans)
+    print(ans)
 
-    if abs(ans[1]) == ans[1]: # Определяет какой знак стоит перед ans[1]
-        plus_minus_1 = '+'
-    else:
-        plus_minus_1 = '-'
-    if abs(ans[2]) == ans[2]: # Определяет какой знак стоит перед ans[2]
-        plus_minus_2 = '+'
-    else:
-        plus_minus_2 = '-'
+    print(f"{round(ans[0])}*x**2 + {round(ans[1])}*x + {round(ans[2])} ") # Выводит уравнения в виде строки 
 
-    print(f"{ans[0]}*x**2 {plus_minus_1} {ans[1]}*x {plus_minus_2} {ans[2]} ") # Ввыводит уравнения в виде строки 
+
+def points_on_a_graph_by_y(x_list: np.ndarray or list,  y_list: np.ndarray or list, gn_coord: list):#  x_lis Это все точки x в функций a y_list Это все точки y в функций.  gn_coord Это список со значения y с x которых вы хотите составить систему уравнения 
+  """ Находит точки пересечения всех x cо списком y который был задан""" 
+
+ 
+  coordinate_x = []
+  coordinate_y = []
+  ypoints = list(y_list)
+  xpoints = list(x_list)
+
+  for coord_y in gn_coord: # Проходим по списку gn_coord
+    x_2 = []
+
+    for y_len in range(len(ypoints)-1): # Проходим по списку ypoints
+
+        if round(ypoints[y_len]) == coord_y: # Сравниваем round(ypoints[y_len]) с coord_y
+          x_2.append(y_len)
+    
+    coordinate_x.append(xpoints[x_2[0]])
+    coordinate_x.append(xpoints[x_2[-1]])
+
+    coordinate_y.append(ypoints[x_2[0]])
+    coordinate_y.append(ypoints[x_2[-1]])
+  return(coordinate_x,coordinate_y)
+gn_coord = [5,10]
+
+x = [] # сюда надо загнать все значения x в графике 
+y = [] # сюда надо загнать все значения y в графике 
+
+
+math_python(function_with_graf(x,y),-6,6,0.0001)
